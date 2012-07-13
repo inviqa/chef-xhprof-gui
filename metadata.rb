3eb4fd67b5b6e8b3a801313b6dbcb32a5fc4a556
Alistair Stead <alistair.stead@designdisclosure.com>
2012-07-13 21:38:50 +0100

    Corrected metadate.rb
    Corrected the meta data definition for dependencies and recipes.

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "php"
-depends "chef-php-extra"
 depends "git"
 depends "mysql"
 depends "database"
@@ -17,3 +16,5 @@ depends "apache2"
 end
 
 recipe "chef-xhprof-gui", "Install and configure XHProf and XHGui by Paul Reinheimer"
+recipe "chef-xhprof-gui::xhprof", "Install and configure XHProf"
+recipe "chef-xhprof-gui::xhgui", "Install and configure XHGui by Paul Reinheimer with MySql database and Apache vhost"
