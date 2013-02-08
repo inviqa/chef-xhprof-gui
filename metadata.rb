63501b6b8b7d30f35ce68dff0d27c5dd468b99c9
Jakub Zalas <jakub@zalas.pl>
2013-02-08 12:04:17 +0000

    Bumped opscode php cookbook version to 1.1.8.

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description      "Installs/Configures XHProf XHGui"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends "php", "= 1.0.2"
+depends "php", "= 1.1.8"
 depends "chef-php-extra"
 depends "git", "= 1.0.0"
 depends "mysql", "= 1.2.6"
