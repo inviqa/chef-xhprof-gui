8a5a6ec1f731eaea0b16daf558e0f68f54f7ba7d
Alistair Stead <alistair.stead@designdisclosure.com>
2013-06-21 14:45:13 +0100

    Update dependencies to update database recipes
    Update dependencoes and use the database::mysql recipe to install
    mysql gem as well as setup the database. This removes the need for
    the basebox to have the dependencies met.

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email "alistair.stead@inviqa.com"
 license          "Apache 2.0"
 description      "Installs/Configures XHProf XHGui"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 
-depends "php", "= 1.1.8"
+depends "php"
 depends "chef-php-extra"
-depends "git", "= 1.0.0"
-depends "mysql", ">= 1.3.0"
-depends "database", "> 1.2.0"
-depends "apache2", "= 1.1.12"
+depends "git"
+depends "mysql"
+depends "database", "> 1.3.0"
+depends "apache2"
 depends "chef-xhprof-gui"
 
 %w{ ubuntu, debian, centos, fedora }.each do |os|
